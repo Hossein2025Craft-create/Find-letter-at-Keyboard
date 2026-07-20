67764eaf8e6a51ffa519137c8220b1fce7055b74
Hossein Hajiarab <bytecraft017@gmail.com>
2026-07-20 16:47:10 +0330

    Unlock your speed! “KeyHunt” is an addictive game designed to boost your typing muscle memory.Challenge: Find the target letter on your keyboard as fast as you can.Level up your touch-typing skills today!

---
+++ Find letter at Keyboard.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblLetter = new System.Windows.Forms.Label();
+            this.lblZaman = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblEmtiaz = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            resources.ApplyResources(this.btnStart, "btnStart");
+            this.btnStart.BackColor = System.Drawing.SystemColors.Control;
+            this.btnStart.Name = "btnStart";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblLetter
+            // 
+            this.lblLetter.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.lblLetter, "lblLetter");
+            this.lblLetter.ForeColor = System.Drawing.Color.White;
+            this.lblLetter.Name = "lblLetter";
+            // 
+            // lblZaman
+            // 
+            resources.ApplyResources(this.lblZaman, "lblZaman");
+            this.lblZaman.Name = "lblZaman";
+            // 
+            // lblTime
+            // 
+            resources.ApplyResources(this.lblTime, "lblTime");
+            this.lblTime.Name = "lblTime";
+            // 
+            // lblEmtiaz
+            // 
+            resources.ApplyResources(this.lblEmtiaz, "lblEmtiaz");
+            this.lblEmtiaz.Name = "lblEmtiaz";
+            // 
+            // lblScore
+            // 
+            resources.ApplyResources(this.lblScore, "lblScore");
+            this.lblScore.Name = "lblScore";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblEmtiaz);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblZaman);
+            this.Controls.Add(this.lblLetter);
+            this.Controls.Add(this.btnStart);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblLetter;
+        private System.Windows.Forms.Label lblZaman;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblEmtiaz;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Find letter at Keyboard.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            Random rnd = new Random();
+            char letter = (char)rnd.Next(65, 91);
+            lblLetter.Text = letter.ToString();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int Code = (int)char.Parse(lblLetter.Text);
+            if (e.KeyValue == Code)
+            {
+                lblScore.Text = (int.Parse(lblScore.Text) + 1).ToString();
+                Console.Beep(1500, 200);
+                Random rnd = new Random();
+                char letter = (char)rnd.Next(65, 91);
+                lblLetter.Text = letter.ToString();
+            }
+            else
+            {
+                timer1.Enabled = false;
+                Console.Beep(500, 1500);
+                MessageBox.Show("شما بازنده شدید");
+                Close();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = (int.Parse(lblTime.Text) - 1).ToString();
+            if (lblTime.Text == "0")
+            {
+                timer1.Enabled = false;
+                int score = int.Parse(lblScore.Text);
+                if (score >= 30)
+                {
+                    MessageBox.Show("عالی");
+                }
+                else if (score >= 25)
+                {
+                    MessageBox.Show("خیلی خوب");
+                }
+                else if (score >= 20)
+                {
+                    MessageBox.Show("خوب");
+                }
+                else if (score >= 15)
+                {
+                    MessageBox.Show("متوسط");
+                }
+                else
+                {
+                    MessageBox.Show("ضعیف");
+                }
+            }
+        }
+    }
+}
